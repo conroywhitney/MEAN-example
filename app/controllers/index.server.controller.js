e19d62af8256492ffb4b4503077c9de43fb04904
Conroy Whitney <conroy.whitney@gmail.com>
2015-02-26 14:51:21 -0500

    ch3 -- index route on post to wtf hahaha

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,7 @@
 exports.render = function(req, res) {
   res.send('Hello, world!');
 }
+
+exports.wtf = function(req, res) {
+  res.send('WTF mate?');
+}
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
   var index = require('../controllers/index.server.controller');
-  app.get('/', index.render);
+  app
+    .get('/', index.render)
+    .post('/', index.wtf)
+  ;
 };
