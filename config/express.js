5d6f56d5199eef101d5bcf39aef0fb763b4f9818
Conroy Whitney <conroy.whitney@gmail.com>
2015-02-26 16:57:50 -0500

    ch3 -- morgan, compress, bodyparser, methodoverride

--- config/express.js
+++ config/express.js
@@ -1,9 +1,28 @@
 var
-  express = require('express')
+  express = require('express'),
+  morgan = require('morgan'),
+  compress = require('compression'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override')
 ;
 
 module.exports = function() {
   var app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
   require('../app/routes/index.server.routes.js')(app);
+
   return app;
 };
