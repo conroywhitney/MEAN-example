0e47fa33cbfc672b494da2cf60c5ce7c63bd4832
Conroy Whitney <conroy.whitney@gmail.com>
2015-02-26 14:42:23 -0500

    ch3 -- chaining middleware for require/validate

--- server-express.js
+++ server-express.js
@@ -1,11 +1,19 @@
 var
   express = require('express'),
+  requireName = function(req, res, next) {
+    if (req.param('name')) {
+      next();
+    } else {
+      res.send('What is your name?');
+    }
+  },
+  helloWorld = function(req, res, next) {
+    res.send('Hello ' + req.param('name'));
+  },
   app = express()
 ;
 
-app.use('/', function(req, res) {
-  res.send('Hello, world!');
-});
+app.get('/', requireName, helloWorld);
 
 app.listen(3000);
 console.log("Express.js server running on port 3000");
